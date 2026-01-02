c544d1e85aecdbe9bbdb3d02c3467b9b095b397e
Akila Madhushanka (KilerZack) <developer.akila5@gmail.com>
2026-01-02 12:46:00 +0530

    feat(learn-python): add basic data types examples and python gitignore

---
+++ Learn-Python/data_type.py
@@ -0,0 +1,29 @@
+v1 = str("Hello World") // String
+v2 = int(20) // Integer
+v3 = float(20.5) // Float
+v4 = complex(1j) // Complex
+v5 = list(("apple", "banana", "cherry")) // List
+v6 = tuple(("apple", "banana", "cherry")) // Tuple
+v7 = range(5) // Range
+v8 = dict(name="John", age=36) // Dictionary
+v9 = set(("apple", "banana", "cherry")) // Set
+v10 = frozenset(("apple", "banana", "cherry")) // Frozenset
+v11 = bool(5) // Boolean
+v12 = bytes(5) // Bytes
+v13 = bytearray(5) // Bytearray
+v14 = memoryview(bytes(5)) // Memoryview
+
+print(type(v1)) // <class 'str'>
+print(type(v2)) // <class 'int'>
+print(type(v3)) // <class 'float'>
+print(type(v4)) // <class 'complex'>
+print(type(v5)) // <class 'list'>
+print(type(v6)) // <class 'tuple'>
+print(type(v7)) // <class 'range'>
+print(type(v8)) // <class 'dict'>
+print(type(v9)) // <class 'set'>
+print(type(v10)) // <class 'frozenset'>
+print(type(v11)) // <class 'bool'>
+print(type(v12)) // <class 'bytes'>
+print(type(v13)) // <class 'bytearray'>
+print(type(v14)) // <class 'memoryview'>
