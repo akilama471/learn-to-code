b96cf309a9dce6ee95acb71bfa34b2f2355c17ef
Akila Madhushanka (KilerZack) <developer.akila5@gmail.com>
2026-01-18 12:46:00 +0530

    feat(pascal): add basic programs for adding numbers and calculating square

---
+++ Learn-Pascal/addingTwoNumbers.pas
@@ -0,0 +1,13 @@
+Program addingTwoNumber;
+Var
+  X,Y,Z:Integer;
+Begin
+     WriteLn('first number');
+		 readln(X);
+		 WriteLn('second number');
+		 Readln(Y);
+		 Z:=X+Y;
+		 Write('Total is : ');
+		 WriteLn(Z);
+		 readln(X);
+ End.
---
+++ Learn-Pascal/square.pas
@@ -0,0 +1,14 @@
+Program square;
+Var
+   X,perimeter,area:integer;
+Begin
+    WriteLn('Padhayaka dhiga');
+		Readln(X);
+		perimeter:=X*4;
+		area:=X*X;
+		WriteLn('perimeter is');
+		WriteLn(perimeter);
+		WriteLn('area is');
+		WriteLn(area);
+		Readln(X);
+End.
