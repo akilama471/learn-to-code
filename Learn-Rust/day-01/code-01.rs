9051f7fae794c257e0ba0901d65fca72d00eaf33
Akila Madhushanka (KilerZack) <developer.akila5@gmail.com>
2026-01-25 19:45:00 +0530

    feat(rust): add day 01 introductory and print macro examples

--- Learn-Rust/day-01/code-01.rs
+++ Learn-Rust/day-01/code-01.rs
@@ -1,3 +1,8 @@
 fn main() {
-  println!("Hello World!");
+    println!("Hello World!");
+    println!("I am learning Rust Programming.");
+    println!("This is my first Rust program.");
+    let name = "Akila";
+    println!("My name is {}", name);
+    println!("This is super awesome!");
 }
---
+++ Learn-Rust/day-01/code-02.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!(" ========= Print Macros ========= ");
+    println!(" ");
+    println!("print!() : print without newline");
+    println!("print!() : You can add new line with \\n in the string");
+    print!("Hello World!\nThis line was broken up!\n");
+    println!(" ");
+    println!("println!() : print and add newline");
+    println!(" ");
+    println!("eprintln!() : print to stderr with newline");
+    println!(" ");
+    println!("format!() : create a string without printing");
+}
