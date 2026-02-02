aeb389667f21be4d5e3f22430600946e391cb0ec
Akila Madhushanka (KilerZack) <developer.akila5@gmail.com>
2026-02-02 19:45:00 +0530

    feat(day-01): add rust comment example script

---
+++ Learn-Rust/day-01/comment.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // This is a comment
+    println!("Hello World!");
+
+    /* The code below will print the words Hello World!
+    to the screen, and it is amazing */
+    println!("Hello World!");
+}
